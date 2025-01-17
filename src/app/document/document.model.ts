fde761e3057cf27a3ed60979cdbe09d5e2515bc3
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-01-16 20:37:44 -0700

    added document model

---
+++ src/app/document/document.model.ts
@@ -0,0 +1,22 @@
+export class Document{
+  // id name description url children
+
+  public id: string
+  public name: string
+  public description: string
+  public url: string
+  public children: Document[]
+
+  constructor(
+    id: string,
+    name: string,
+    description: string,
+    url: string,
+    children: Document[]){
+      this.id = id
+      this.name = name
+      this.description = description
+      this.url = url
+      this.children = children
+    }
+}
