49491ed76aabfb0477f88e8da93ec5c6cae177ab
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-01-20 16:30:54 -0700

    feat: assignment 4.1 complete

--- src/app/document/document-item/document-item.component.ts
+++ src/app/document/document-item/document-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-item',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './document-item.component.scss'
 })
 export class DocumentItemComponent {
+
+  @Input() document: Document
 
 }
