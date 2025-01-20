f3e08dc1beb73361730ad24c876f2e40004c9450
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-01-20 16:39:19 -0700

    feat: completed 4.2 assignment

--- src/app/document/document-list/document-list.component.ts
+++ src/app/document/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.component.scss'
 })
 export class DocumentListComponent {
+
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documentList: Document[] = [
+    new Document('1', 'TestDoc1', 'This is the FIRST test', 'https://www.linkedin.com/in/wesleyvane1', []),
+    new Document('2', 'TestDoc2', 'This is the SECOND test', 'https://www.linkedin.com/in/wesleyvane1', []),
+    new Document('3', 'TestDoc3', 'This is the THIRD test', 'https://www.linkedin.com/in/wesleyvane1', []),
+    new Document('4', 'TestDoc4', 'This is the FOURTH test', 'https://www.linkedin.com/in/wesleyvane1', [])
+  ]
+
+  onSelect(document: Document){
+    this.selectedDocumentEvent.emit(document)
+  }
 
 }
