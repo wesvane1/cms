8e25f833d08afd1e4ffdadfb342d40634e9b87b4
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-02-04 11:28:57 -0700

    chore: minor change to the model for the new mock contacts

--- src/app/contact/contact.model.ts
+++ src/app/contact/contact.model.ts
@@ -1,30 +1,24 @@
-export class Contact{
+export class Contact {
   public id: string;
   public name: string;
   public email: string;
   public phone: string;
   public imageUrl: string;
-  public group: string;
+  public group: Contact[] | null;
 
   constructor(
-    id:string, 
+    id: string, 
     name: string, 
     email: string, 
     phone: string, 
     imageUrl: string, 
-    group: string){
-
-      
-    this.id = id
-    this.name = name
-    this.email = email
-    this.phone = phone
-    this.imageUrl = imageUrl
-    this.group = group
-  }
-
-  getFormattedName(): string {
-    const lastName = this.name.split(' ').pop();
-    return `Bro. ${lastName}`;
+    group: Contact[] | null
+  ) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.imageUrl = imageUrl;
+    this.group = group;
   }
 }
