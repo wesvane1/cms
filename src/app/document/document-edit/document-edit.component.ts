bf8c482cf4b706820c915b5f66f2b21584667b87
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-02-11 17:36:23 -0700

    6.4: Create the DocumentEditComponent

---
+++ src/app/document/document-edit/document-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-edit',
+  standalone: false,
+  
+  templateUrl: './document-edit.component.html',
+  styleUrl: './document-edit.component.scss'
+})
+export class DocumentEditComponent {
+
+}
