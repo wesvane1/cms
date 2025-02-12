a7947c036b7943cf6f1e691ff8f390e238718030
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-02-11 17:25:38 -0700

    6.1: Define the Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DocumentComponent } from './document/document.component';
+import { ContactComponent } from './contact/contact.component';
+import { MessageListComponent } from './message/message-list/message-list.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/documents', pathMatch: 'full'},
+  {path: '/documents', component: DocumentComponent},
+  {path: '/messages', component: MessageListComponent},
+  {path: '/contacts', component: ContactComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
