db060bb04d497a768f9653c38f95a0438d191e65
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-02-11 17:34:09 -0700

    6.3: Define [routerLink] Directives for Each Route

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +8,5 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  @Output() selectedFeatureEvent = new EventEmitter<string>();
 
-  onSelected(selectedEvent: string){
-    this.selectedFeatureEvent.emit(selectedEvent)
-  }
 }
