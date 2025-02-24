31ef7b3a9dad930ce0e0a97ff23247e7d0695fd6
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-02-24 16:58:57 -0700

    8.3: Implementing the Behavior of the Document Edit Form

--- src/app/document/document-edit/document-edit.component.ts
+++ src/app/document/document-edit/document-edit.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { DocumentService } from '../document.service';
 import { Document } from '../document.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-document-edit',
@@ -10,7 +11,7 @@ import { Document } from '../document.model';
   templateUrl: './document-edit.component.html',
   styleUrl: './document-edit.component.scss'
 })
-export class DocumentEditComponent {
+export class DocumentEditComponent implements OnInit{
 
   originalDocument: Document;
   document: Document;
@@ -19,12 +20,54 @@ export class DocumentEditComponent {
   constructor(
     private documentService: DocumentService,
     private router: Router,
+    private route: ActivatedRoute,
   ){}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id'];
+  
+      if (!id) {
+        this.editMode = false;
+        return;
+      }
+  
+      this.originalDocument = this.documentService.getDocument(id);
+  
+      if (!this.originalDocument) {
+        return;
+      }
+  
+      this.editMode = true;
+      this.document = { ...this.originalDocument }; // Cloning the object
+    });
+  }
+
+  onSubmit(form: NgForm){
+    const value = form.value
+    let newDocument = new Document(
+      value.id,
+      value.name,
+      value.description,
+      value.url,
+      value.children
+    )
+    if(this.editMode){
+      this.documentService.updateDocument(this.originalDocument, newDocument)
+      this.onUpdate()
+    } else{
+      this.documentService.addDocument(newDocument)
+      this.onCreate()
+    }
+  }
 
   onCreate(){
     this.router.navigate(['/documents'])
   }
   onUpdate(){
+    this.router.navigate(['/documents'])
+  }
+  onCancel(){
     this.router.navigate(['/documents'])
   }
 }
