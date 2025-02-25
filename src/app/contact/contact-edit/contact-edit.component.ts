138638bb17f6cfcacc977acbe1c8ea3f75db4735
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-02-25 08:39:46 -0700

    8.7: Implementing the Behavior of the Contact Edit Form

--- src/app/contact/contact-edit/contact-edit.component.ts
+++ src/app/contact/contact-edit/contact-edit.component.ts
@@ -1,20 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ContactService } from '../contact.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'app-contact-edit',
   standalone: false,
-  
+
   templateUrl: './contact-edit.component.html',
   styleUrl: './contact-edit.component.scss'
 })
-export class ContactEditComponent {
+export class ContactEditComponent implements OnInit{
+
+  originalContact: Contact;
+  contact: Contact;
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id: string;
 
   constructor(
     private contactService: ContactService,
     private router: Router,
+    private route: ActivatedRoute,
   ){}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id']
+      if (!id){
+        this.editMode = false
+        return;
+      }
+      this.originalContact = this.contactService.getContact(id)
+
+      if (!this.originalContact){
+        return
+      }
+      this.editMode = true;
+      this.contact = {...this.originalContact}
+
+      if(this.contact.group){
+        this.groupContacts = {...this.contact.group}
+      }
+    })
+  }
+
+  onSubmit(form: NgForm){
+
+  }
 
   onCreate(){
     this.router.navigate(['/contacts'])
