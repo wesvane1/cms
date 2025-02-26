9603844755a96ff7f5d4755ce648b384fdf6484e
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-02-26 12:07:00 -0700

    Completed W09: Modify the MessageService to Perform HTTP Get/Put Requests

--- src/app/message/message.service.ts
+++ src/app/message/message.service.ts
@@ -1,6 +1,8 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Message } from './message.model';
-import { MOCKMESSAGES } from './MOCKMESSAGES';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Subject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +13,12 @@ export class MessageService {
   messageChangedEvent = new EventEmitter<Message[]>();
   
   private messages: Message[] = []
+  private maxMessageId = 0;
 
-  constructor() {
-    this.messages = MOCKMESSAGES
-  }
-
-  getMessages(): Message[]{
-    return this.messages.slice()
+  constructor(
+    private http: HttpClient,
+  ) {
+    this.messages = this.getMessages()
   }
   
   getMessage(id: string): Message{
@@ -30,7 +31,50 @@ export class MessageService {
   }
 
   addMessage(message: Message){
+    if(!message){
+      return
+    }
+    this.maxMessageId++
+    message.id = (this.maxMessageId).toString()
     this.messages.push(message);
-    this.messageChangedEvent.emit(this.messages.slice())
+    // this.messageChangedEvent.emit(this.messages.slice())
+    this.storeMessages()
+  }
+  getMaxId(): number{
+    let maxId = 0
+
+    for (const message of this.messages){
+      if (+message.id > maxId){
+        maxId = +message.id
+      }
+    }
+    return maxId
+  }
+
+  getMessages(): Message[]{
+    this.http.get<Message[]>('https://cms-9f75e-default-rtdb.firebaseio.com/messages.json').subscribe((messages: Message[]) => {
+      this.messages = messages
+      this.maxMessageId = this.getMaxId()
+
+      this.messages.sort((a, b) => (a.sender < b.sender ? -1 : a.sender > b.sender ? 1 : 0));
+      this.messageChangedEvent.next(this.messages.slice());
+
+    }, (error: any) => {
+      console.error('Error fetching messages: ', error)
+    })
+    return this.messages.slice()
+  }
+
+  storeMessages(){
+    const messagesJSON = JSON.stringify(this.messages);
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    this.http.put(
+      'https://cms-9f75e-default-rtdb.firebaseio.com/messages.json',
+      messagesJSON,
+      {headers}
+      ).subscribe(() => {
+      const messageListClone = this.messages.slice()
+      this.messageChangedEvent.next(messageListClone);
+    })
   }
 }
