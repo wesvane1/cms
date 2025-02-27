5c2fed67b0e88a1b1f9c13e40cff2b488a0c1701
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-02-27 07:09:46 -0700

    fix: correct route added for non-defined routes

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use('/documents', documentRoutes);
 
 // Tell express to map all other non-defined routes back to the index page
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/cms/broswer/index.html'));
+  res.sendFile(path.join(__dirname, '../cms/src/index.html'));
 });
 
 // Define the port address and tell express to use this port
