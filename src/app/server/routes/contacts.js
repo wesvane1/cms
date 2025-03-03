ad80436a6a3f427a48049d2464c3eb8ec9bed89e
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-03-03 11:54:07 -0700

    11.10: Mapping Referenced Objects

--- src/app/server/routes/contacts.js
+++ src/app/server/routes/contacts.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-  Contact.find().then(contacts => {
+  Contact.find().populate().then(contacts => {
     res.status(200).json(contacts)
   }).catch(err => {
     res.status(500).json({message: 'Error retrieving contacts', error: err})
