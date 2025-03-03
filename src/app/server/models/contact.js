d7d6f279c26fe09e472ff51586d8ae383cc8bd9e
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-03-03 11:28:57 -0700

    11.5: Create the Document, Message, Contact, and Sequence Models.

---
+++ src/app/server/models/contact.js
@@ -0,0 +1,13 @@
+
+const mongoose = require('mongoose');
+
+const contactSchema = mongoose.Schema({
+   id: { type: String, required: true },
+   name: { type: String, required: true },
+   email: { type: String, required: true },
+   phone: { type: String, required: true },
+   imageUrl: { type: String }, 
+   group: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Contact' }]
+});
+
+module.exports = mongoose.model('Contact', contactSchema);
